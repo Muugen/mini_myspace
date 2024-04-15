a9da2cc2a43be9551e4e8885414447ca51c855e6
Anthony HUYNH <anthonyhuynh1204@gmail.com>
2024-04-15 08:12:30 +0200

    fix: redirection to signin

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-import styles from "./page.module.css";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-const Home = () => {
+export default async function Home() {
+  const session = await getServerSession();
+
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+
   return <main></main>;
-};
-
-export default Home;
+}
